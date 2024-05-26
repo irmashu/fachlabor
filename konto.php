71a252b6e50efd3135a6a340b63a1705bdc6887b
irmashu <shurawljow@gmail.com>
2024-05-26 17:23:46 +0200

    Unerfolgreicher Versuch Fix Bestellsumme

--- konto.php
+++ konto.php
@@ -24,7 +24,7 @@ $db = new DBConnector($DBServer, $DBHost, $DBUser, $DBPassword);
 $db->connect();
 
 // Construct the query for the data that we want to see
-$query = 'SELECT bestellung.Bestelldatum, bestellung.BestellNr, SUM(sku.Preis) AS Bestellsumme, auftrag.Status';
+$query = 'SELECT DISTINCT bestellung.Bestelldatum, bestellung.BestellNr, SUM(sku.Preis * bestellposten.Quantität) AS Bestellsumme, auftrag.Status';
 $query .= ' FROM bestellung';
 $query .= ' LEFT JOIN gehört_zu ON bestellung.BestellNr = gehört_zu.BestellNr';
 $query .= ' LEFT JOIN auftrag ON gehört_zu.AuftragsNr = auftrag.AuftragsNr';
@@ -54,13 +54,13 @@ $result = $db->getEntityArray($query);
     <h1>Willkommen im AirLimited Shop!</h1>
     <nav>
         <button onclick="window.location.href='index.php'">Onlineshop</button>
-        <button onclick="window.location.href='fertigung.html'" class="fertigung-btn">Fertigung</button>
+        <button onclick="window.location.href='fertigung.php'" class="fertigung-btn">Fertigung</button>
         <button onclick="window.location.href='management.php'" class="management-btn">Management</button>
         <button onclick="window.location.href='login.php'" class="login-btn">Anmelden</button>
     </nav>
     <div class="account-buttons">
         <button onclick="window.location.href='konto.php'">Mein Konto</button>
-        <button onclick="window.location.href='warenkorb.html'">Warenkorb</button>
+        <button onclick="window.location.href='warenkorb.php'">Warenkorb</button>
     </div>
     <div class="meine-logindaten">
         <p>
@@ -87,14 +87,16 @@ $result = $db->getEntityArray($query);
         <tbody>
             <?php
             if ($result) {
+
                 foreach ($result as $bestellung) {
                     echo '<tr>';
                     echo '<td>' . $bestellung->Bestelldatum . '</td>';
                     echo '<td>' . $bestellung->BestellNr. '</td>';
-                    echo '<td>' . number_format($bestellung->Bestellsumme, 2, ',', '.') . ' €</td>';
+                    echo '<td>' . $bestellung->Bestellsumme.' €</td>';
                     echo '<td>' . $bestellung->Status . '</td>';
                     echo '<td><a href="bestelldetails.php?BestellNr=' . urlencode($bestellung->BestellNr) . '">Details anzeigen</a></td>';
                     echo '</tr>';
+                  
                 }
             } else {
                 echo '<tr><td colspan="5">Keine Bestellungen gefunden.</td></tr>';
